105064d348ba641e5866661af79e6f66de25a28b
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-04-26 16:51:05 +0200

    Init `cw07` and `cw08`

---
+++ cw07/lab/posix_sender.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <mqueue.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define BLOCK_SIZE 1024
+
+int main(int argc, char **argv) {
+    int fd = shm_open("/name", O_CREAT | O_RDWR, 0644);
+    ftruncate(fd, BLOCK_SIZE);
+    char *ptr = (char *) mmap(NULL, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    strcpy(ptr, "hello");
+
+    munmap(ptr, BLOCK_SIZE);
+    // shm_unlink("/name");
+
+    return 0;
+}
