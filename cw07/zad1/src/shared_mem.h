f1f79d9ade8045ba572f8943811b88da2b9bf90a
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-02 19:12:04 +0200

    Fix `semaphore` and `shared_sem` [`cw07`]

--- cw07/zad1/src/shared_mem.h
+++ cw07/zad1/src/shared_mem.h
@@ -5,7 +5,7 @@
 
 int create_shared_mem(const char*, int);
 int open_shared_mem(const char*);
-char* set_shared_mem(const char*, int);
+char* attach_shared_mem(const char*);
 bool detach_shared_mem(char*);
 bool delete_shared_mem(const char*);
 
